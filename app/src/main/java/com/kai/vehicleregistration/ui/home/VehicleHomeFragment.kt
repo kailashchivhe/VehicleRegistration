5486b21dd8ad5acbb5cc71e4a702763f10a48a40
Kailash Chivhe <kchivhe@gmail.com>
2021-05-15 19:03:29 +0530

    chore ( Package ) : Refactored existing UI packages.

--- app/src/main/java/com/kai/vehicleregistration/ui/home/VehicleHomeFragment.kt
+++ app/src/main/java/com/kai/vehicleregistration/ui/home/VehicleHomeFragment.kt
@@ -1,22 +1,23 @@
-package com.kai.vehicleregistration
+package com.kai.vehicleregistration.ui.home
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.kai.vehicleregistration.R
 
-
-class NewVehicleFragment : Fragment() {
-
+class VehicleHomeFragment : Fragment()
+{
     override fun onCreateView( inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle? ): View?
     {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_new_vehicle, container, false)
+        return inflater.inflate(R.layout.fragment_vehicle_home, container, false)
     }
 
     override fun onViewCreated( view: View, savedInstanceState: Bundle? )
     {
         super.onViewCreated(view, savedInstanceState)
+
     }
 }
--- app/src/main/java/com/kai/vehicleregistration/ui/register/NewVehicleFragment.kt
+++ app/src/main/java/com/kai/vehicleregistration/ui/register/NewVehicleFragment.kt
@@ -1,22 +1,23 @@
-package com.kai.vehicleregistration
+package com.kai.vehicleregistration.ui.register
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.kai.vehicleregistration.R
 
-class VehicleHomeFragment : Fragment()
-{
+
+class NewVehicleFragment : Fragment() {
+
     override fun onCreateView( inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle? ): View?
     {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_vehicle_home, container, false)
+        return inflater.inflate(R.layout.fragment_new_vehicle, container, false)
     }
 
     override fun onViewCreated( view: View, savedInstanceState: Bundle? )
     {
         super.onViewCreated(view, savedInstanceState)
-
     }
 }
